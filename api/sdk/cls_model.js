cb967610ca6eeae9bd4a8c480cdb294e4bda01d3
Afaizin-bitt <84916753+Afaizin-bitt@users.noreply.github.com>
2021-06-04 02:50:53 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -3,8 +3,8 @@ const tf = require('@tensorflow/tfjs-node');
 function normalized(data){ // i & r
     i = (data[0] - 12.585) / 6.813882
     r = (data[1] - 51.4795) / 29.151289
-    v = (data[1] - 650.4795) / 552.6351
-    p = (data[1] - 10620.56) / 12152.78
+    v = (data[2] - 650.4795) / 552.6351
+    p = (data[3] - 10620.56) / 12152.78
 
     return [i, r, v, p]
 }
@@ -12,16 +12,16 @@ function normalized(data){ // i & r
 const argFact = (compareFn) => (array) => array.map((el, idx) => [el, idx]).reduce(compareFn)[1]
 const argmax = argFact ((min, el) => (el[0] > min[0] ? el : min))
 
-function Argmax(res){
+function ArgMax(res){
   label = "NORMAL"
     cls_data = []
-  if (i=0; i<res.length; i++){
+  for (i=0; i<res.length; i++){
     cls_data[i] = res[i]
   }
     console.log(cls_data, argMax(cls_data));
     if(argMax(cls_data) == 1){
       label = "OVER VOLTAGE"
-    }if (argmax(cls_data) == 0){
+    }if (argMax(cls_data) == 0){
       label = "DROP VOLTAGE"
     }
     return label
