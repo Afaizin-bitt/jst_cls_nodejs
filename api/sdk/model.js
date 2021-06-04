33519999f53effbafdd62825301cb830c996d309
Afaizin-bitt <84916753+Afaizin-bitt@users.noreply.github.com>
2021-06-04 10:06:40 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -23,7 +23,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/Afaizin-bitt/jst_cls_nodejs/main/api/sdk/model.js';
+        const path = 'https://raw.githubusercontent.com/Afaizin-bitt/jst_cls_nodejs/main/public/ex_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
